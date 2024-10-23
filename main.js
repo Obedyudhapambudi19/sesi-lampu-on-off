d01cfee11d21649354e5ede177860c4f048f7a85
Obedyudhapambudi19 <115157556+Obedyudhapambudi19@users.noreply.github.com>
2024-10-24 00:05:21 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function saklarOn() {
+    console.log('testing saklar')
+    let lampu1 = document.getElementById('lampu1')
+    console.log(lampu1.src)
+    lampu1.src='assets/images/on.gif'
+
+}
+
+function saklarOff() {
+    console.log('testing saklar')
+    lampu1.src ='assets/images/off.gif'
+}
